f76b3fa1e709652e5c6189faf79afcd7d1eabd05
Hannah Maratea <maratea101@gmail.com>
2022-12-03 16:48:45 -0500

    commit tree 500dbd32843599e9ec4bc67161244b3ba64a2882

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased () {
+ console.log(tutorials);
+
+ const titles = tutorials.map ((e) => {
+  return e.split(' ').map((word) => {
+  return word.charAt(0).toUpperCase() + word.slice(1)
+  }).join(' ')
+ }) 
+ return titles
 }
+
+
+
